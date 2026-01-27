6569ad2919a9f27eb07fd05c46d0f8f198b96861
ychen-develop <19857682897@163.com>
2026-01-27 19:29:33 +0800

    UI beautification

---
+++ STM32物联网智能家居/APP/P4界面美化/pages/index/index.vue
@@ -0,0 +1,145 @@
+<template>
+	<view class="wrap">
+		<view class="dev-area">
+
+			<view class="dev-cart">
+				<view class="">
+					<view class="dev-name">温度</view>
+					<image class="dev-logo" src="../../static/logo.png" mode=""></image>
+				</view>
+				<view class="dev-data">{{temp}} ℃</view>
+			</view>
+			<view class="dev-cart">
+				<view class="">
+					<view class="dev-name">湿度</view>
+					<image class="dev-logo" src="../../static/logo.png" mode=""></image>
+				</view>
+				<view class="dev-data">{{humi}} ℃</view>
+			</view>
+			<view class="dev-cart">
+				<view class="">
+					<view class="dev-name">台灯</view>
+					<image class="dev-logo" src="../../static/logo.png" mode=""></image>
+				</view>
+				<switch :checked="led" @change="onLedSwitch" color="#8f8f94"/>
+			</view>
+
+		</view>
+	</view>
+
+</template>
+
+<script>
+	const {
+		createCommonToken
+	} = require('@/key.js')
+	export default {
+		data() {
+			return {
+				temp: '',
+				humi: '',
+				led: true,
+				token: '',
+			}
+		},
+		onLoad() {
+			const params = {
+				access_key: 't/vVbWvTSz9br6mxx13BC131dc+1Nx2Tw1nz6ZWHI3U',
+				version: '2022-05-01',
+				productid: 'Aio2o842Wl',
+			}
+			this.token = createCommonToken(params);
+		},
+		onShow() {
+			this.fetchDevData();
+			// setInterval(()=>{
+			// 	this.fetchDevData();
+			// },3000)
+		},
+		methods: {
+			fetchDevData() {
+				uni.request({
+					url: 'https://iot-api.heclouds.com/thingmodel/query-device-property', 
+					method: 'GET',
+					data: {
+						product_id: 'Aio2o842Wl',
+						device_name: 'd1'
+					},
+					header: {
+						'authorization': this.token 
+					},
+					success: (res) => {
+						console.log(res.data);
+						this.temp = res.data.data[2].value;
+						this.humi = res.data.data[0].value;
+						this.led = res.data.data[1].value === 'true';
+
+					}
+				});
+			},
+			onLedSwitch(event) {
+				console.log(event.detail.value);
+				let value = event.detail.value;
+				uni.request({
+					url: 'https://iot-api.heclouds.com/thingmodel/set-device-property', 
+					method: 'POST',
+					data: {
+						product_id: 'Aio2o842Wl',
+						device_name: 'd1',
+						params: {
+							"led": value
+						}
+					},
+					header: {
+						'authorization': this.token 
+					},
+					success: () => {
+						console.log('LED ' + (value ? 'ON' : 'OFF') + ' !');
+					}
+				});
+			}
+
+		}
+	}
+</script>
+
+<style>
+	.wrap {
+		padding: 30rpx;
+	}
+
+	.dev-area {
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+	}
+
+	.dev-cart {
+		height: 150rpx;
+		width: 320rpx;
+		border-radius: 30rpx;
+		margin-top: 30rpx;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+		box-shadow: 0 0 15rpx #ccc;
+	}
+
+	.dev-name {
+		font-size: 20rpx;
+		text-align: center;
+		color: #6d6d6d;
+	}
+
+	.dev-logo {
+		width: 70rpx;
+		height: 70rpx;
+		margin-top: 10rpx;
+	}
+
+	.dev-data {
+		font-size: 50rpx;
+		color: #6d6d6d;
+	}
+
+</style>
